9b15131ebb3d8bae7d7a235162563b1292a1629f
Max Worrall <max.worralL@education.nsw.gov.au>
2023-09-02 09:46:27 +1000

    Added nsurface thing for button!

--- util/button.py
+++ util/button.py
@@ -15,6 +15,9 @@ class Button:
         self.txt = txt
         self.font = font
         self.txtcolour = txtcolour
+        c = screen.copy()
+        self.update(0, 0)
+        screen = c
     
     def __str__(self):
         return 'Button saying "%s"' % self.txt
@@ -31,14 +34,17 @@ class Button:
         bool
             whether or not the user has their mouse ___***OVER***___ the button, NOT CLICKED.
         """
-        txts = []
-        h = 10
-        for line in renderTextCenteredAt(self.txt, self.font, self.max_width):
-            r = self.font.render(line, True, self.txtcolour)
-            txts.append((r, (x+10, y+h)))
-            h += r.get_height() + 10
-        
-        btn = pygame.Rect(x, y, max([i[0].get_width() for i in txts])+20, h)
+        lines = [self.font.render(line, True, self.txtcolour) for line in renderTextCenteredAt(self.txt, self.font, self.max_width)]
+
+        self.nsurface = pygame.Surface((max([i.get_width() for i in lines])+20, sum([i.get_height() for i in lines])+(len(lines)+1)*10))
+        col = (self.txtcolour[0], self.txtcolour[1], (self.txtcolour[2]+1 if self.txtcolour[2] < 255 else self.txtcolour[2]-1))
+        self.nsurface.fill(col)
+        top = 10
+        for i in lines:
+            self.nsurface.blit(i, (10, top))
+            top += i.get_height()+10
+
+        btn = pygame.Rect(x, y, *self.nsurface.get_size())
         pygame.draw.rect(self.screen, self.colour, btn, border_radius=self.roundness)
-        for i in txts: self.screen.blit(*i)
+        self.screen.blit(self.nsurface, (x, y))
         return btn.collidepoint(pygame.mouse.get_pos())

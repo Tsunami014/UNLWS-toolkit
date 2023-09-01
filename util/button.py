043694de2aa0cab8a521c4bc2c5a84df0922e557
Max Worrall <max.worralL@education.nsw.gov.au>
2023-09-01 16:10:13 +1000

    text-wrap buttons!

--- util/button.py
+++ util/button.py
@@ -1,14 +1,20 @@
+try:
+    from util.text import *
+except ImportError:
+    from text import *
+
 import pygame
 pygame.init()
 
 class Button:
-    def __init__(self, screen, txt, colour, txtcolour=(255, 255, 255), font=pygame.font.Font(None, 24), roundness=8):
+    def __init__(self, screen, txt, colour, txtcolour=(255, 255, 255), max_width=100, font=pygame.font.Font(None, 24), roundness=8):
         self.screen = screen
-        self.button = pygame.Rect(0, 0, 200, 30)
         self.roundness = roundness
         self.colour = colour
+        self.max_width = max_width
         self.txt = txt
-        self.text = font.render(txt, True, txtcolour)
+        self.font = font
+        self.txtcolour = txtcolour
     
     def __str__(self):
         return 'Button saying "%s"' % self.txt
@@ -25,8 +31,14 @@ class Button:
         bool
             whether or not the user has their mouse ___***OVER***___ the button, NOT CLICKED.
         """
-        btn = self.button.copy()
-        btn.move_ip(x, y)
+        txts = []
+        h = 10
+        for line in renderTextCenteredAt(self.txt, self.font, self.max_width):
+            r = self.font.render(line, True, self.txtcolour)
+            txts.append((r, (x+10, y+h)))
+            h += r.get_height() + 10
+        
+        btn = pygame.Rect(x, y, max([i[0].get_width() for i in txts])+20, h)
         pygame.draw.rect(self.screen, self.colour, btn, border_radius=self.roundness)
-        self.screen.blit(self.text, (btn.left+10, btn.top + 10))
+        for i in txts: self.screen.blit(*i)
         return btn.collidepoint(pygame.mouse.get_pos())

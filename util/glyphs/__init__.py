d275bb17785e71675e62a06514a5ffcbb27bc5c7
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-05-23 13:59:37 +1000

    Replaced the crappy self-made function for parsing the svg file with a more superior library

--- util/glyphs/__init__.py
+++ util/glyphs/__init__.py
@@ -1,5 +1,7 @@
 import pygame
 from xml.dom import minidom
+from svg.path import parse_path
+
 class Glyph:
     def __init__(self, name: str, points: list[tuple[int,int]] = None) -> None:
         self.name = name
@@ -30,51 +32,15 @@ def _getGlyph(name: str) -> list[tuple[int,int]]:
     return _correctGlyph(_parseGlyph(allGlyphs[name].attributes.get('d').value))
 
 def _parseGlyph(ptsstr: str) -> list[tuple[int,int]]:
-    cmd = None
-    typ = None
+    path = parse_path(ptsstr)
     points = []
-    for i in ptsstr.split(' '):
-        if len(i) == 1:
-            typ = (-1 if i == i.upper() else 1)
-            i = i.upper()
-            if not i in ['M', 'V', 'H', 'L']:
-                raise ValueError(
-                    'Unable to parse glyph: Incorrectly structured path - command not known!'
-                )
-            cmd = i
-        else:
-            if cmd is None:
-                raise ValueError(
-                    'Unable to parse glyph: Incorrectly structured path - command does not exist!'
-                )
-            if cmd == 'M':
-                points.append(tuple([float(j) for j in i.split(',')]))
-            elif cmd == 'V':
-                if len(points) == 0:
-                    raise ValueError(
-                        'Unable to parse glyph: Incorrectly structured path - Cannot move vertically if there\'s nothing to move up from!'
-                    )
-                points.append((points[-1][0], points[-1][1] + (float(i) * typ)))
-            elif cmd == 'H':
-                if len(points) == 0:
-                    raise ValueError(
-                        'Unable to parse glyph: Incorrectly structured path - Cannot move horizontally if there\'s nothing to move horizontally from!'
-                    )
-                points.append((points[-1][0] + (float(i) * typ), points[-1][1]))
-            elif cmd == 'L':
-                if len(points) == 0:
-                    raise ValueError(
-                        'Unable to parse glyph: Incorrectly structured path - Cannot move if there\'s nothing to move from!'
-                    )
-                i = i.split(',')
-                points.append((points[-1][0] + (float(i[0]) * typ), points[-1][1] + (float(i[1]) * typ)))
+    for segment in path:
+        points.append((segment.start.real, segment.start.imag))
+        points.append((segment.end.real, segment.end.imag))
     return points
-            
 
 def _correctGlyph(points: list[tuple[int,int]]) -> list[tuple[int,int]]:
     mi = (min([i[0] for i in points]), min([i[1] for i in points]))
     newpts = [(i[0] - mi[0], i[1] - mi[1]) for i in points]
     ma = max([max([i[0] for i in newpts]), max([i[1] for i in newpts])])
     return [(i[0] / ma, i[1] / ma) for i in newpts]
-
-pass

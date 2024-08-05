3e47e92758e459d1da1ecc7376f43ac0cb74d6b1
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-08-06 09:38:08 +1000

    Made the dragging much better!
    It only drags on click and saves the spot of the click for an offset!

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // TODO: Auto add all the glyphs in the glyphs folder instead of make them manually on the HTML
 
+var offset = [0,0];
+var reload = true;
+
 document.body.onload = function() {
     var it = document.getElementById("glyph")
+    it.ondragstart = function() { return false; };
     it.addEventListener('mousemove', function(event) {
-        console.log("MOUSEMOVE")
         const x = event.clientX;
         const y = event.clientY;
-        it.style = `top: ${y};left: ${x}`
+        var flags = event.buttons !== undefined ? event.buttons : event.which;
+        if ((flags & 1) === 1) {
+            if (reload) {
+                // Set offset to mouse position - current position of object, if set
+                offset = [x - it.offsetLeft, y - it.offsetTop];
+                reload = false;
+            }
+            it.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
+        }
     });
+    it.addEventListener("mouseup", function(event){reload = true;});
 }

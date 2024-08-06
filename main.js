da4b56254f468b170f4bc517069890eef94696ef
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-08-06 15:48:19 +1000

    Made you able to cleanly make new glyphs by dragging them out of the sidebar!

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ var reload = true;
 var current = null;
 var cover;
 
-function addGlyph(glyph) {
+function addGlyph(glyph, position=[0,0]) {
     var it = document.createElement("img");
     it.classList.add("glyph");
     it.src = `glyphs/${glyph}.svg`;
     it.width = 100;
     it.height = 100;
+    it.style.left = position[0];
+    it.style.top = position[1];
     it.ondragstart = function() { return false; };
     it.addEventListener('mousemove', function(event) {
         const x = event.clientX;
@@ -21,12 +23,14 @@ function addGlyph(glyph) {
                 current = it;
                 cover.style = "display: block;";
             }
-            if (reload) {
-                // Set offset to mouse position - current position of object, if set
-                offset = [x - it.offsetLeft, y - it.offsetTop];
-                reload = false;
+            if (current === it) {
+                if (reload) {
+                    // Set offset to mouse position - current position of object, if set
+                    offset = [x - it.offsetLeft, y - it.offsetTop];
+                    reload = false;
+                }
+                it.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
             }
-            it.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
         }
     });
     it.addEventListener("mouseup", function(event){
@@ -34,6 +38,40 @@ function addGlyph(glyph) {
         reload = true;
         current = null;
     })
+    return it;
+}
+
+var sidebarh = 10;
+
+function addGlyphToSidebar(glyph) {
+    var sidebar = document.getElementById("sidebar");
+    // Make a new glyph and centre it on the sidebar
+    var it = document.createElement("img");
+    it.classList.add("glyph");
+    it.src = `glyphs/${glyph}.svg`;
+    it.width = 100;
+    it.height = 100;
+    it.style.left = (sidebar.clientWidth - 100) / 2;
+    it.style.top = sidebarh;
+    it.ondragstart = function() { return false; };
+    it.addEventListener('mousemove', function(event) {
+        const x = event.clientX;
+        const y = event.clientY;
+        var flags = event.buttons !== undefined ? event.buttons : event.which;
+        if ((flags & 1) === 1) {
+            if (current === null) {
+                var bbox = it.getBoundingClientRect();
+                var newit = addGlyph(glyph, [bbox.left, bbox.top]);
+                offset = [x - bbox.left, y - bbox.top];
+                reload = false;
+                current = newit;
+                document.getElementById("main").appendChild(newit);
+                cover.style = "display: block;";
+            }
+        }
+    });
+    sidebar.appendChild(it);
+    sidebarh += 110;
     return it;
 }
 
@@ -52,5 +90,15 @@ document.body.onload = function() {
             current.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
         }
     })
-    document.getElementById("sidebar").appendChild(addGlyph("you"));
+    fetch('glyphs/').then(resp=>{resp.text().then(txt=>{
+        var el = document.createElement('html');
+        el.innerHTML = txt;
+        var files = el.getElementsByTagName('a');
+        for (var i = 0;i < files.length;i++) {
+            var href = files[i].href;
+            if (href.endsWith('.svg')) {
+                addGlyphToSidebar(href.slice(href.lastIndexOf("/")+1, href.lastIndexOf(".")));
+            }
+        }
+    })});
 }

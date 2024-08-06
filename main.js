532da03c394425fef1a258fcf8c7d6db7c69bb23
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-08-06 13:48:48 +1000

    Added a sidebar and made an add glyph function!

--- main.js
+++ main.js
@@ -5,22 +5,12 @@ var reload = true;
 var current = null;
 var cover;
 
-document.body.onload = function() {
-    cover = document.getElementById("cover");
-    cover.style = "display: none;";
-    cover.addEventListener("mouseup", function(event){
-        cover.style = "display: none;";
-        reload = true;
-        current = null;
-    })
-    cover.addEventListener("mousemove", function(event){
-        if (current !== null) {
-            const x = event.clientX;
-            const y = event.clientY;
-            current.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
-        }
-    })
-    var it = document.getElementById("glyph")
+function addGlyph(glyph) {
+    var it = document.createElement("img");
+    it.classList.add("glyph");
+    it.src = `glyphs/${glyph}.svg`;
+    it.width = 100;
+    it.height = 100;
     it.ondragstart = function() { return false; };
     it.addEventListener('mousemove', function(event) {
         const x = event.clientX;
@@ -44,4 +34,23 @@ document.body.onload = function() {
         reload = true;
         current = null;
     })
+    return it;
+}
+
+document.body.onload = function() {
+    cover = document.getElementById("cover");
+    cover.style = "display: none;";
+    cover.addEventListener("mouseup", function(event){
+        cover.style = "display: none;";
+        reload = true;
+        current = null;
+    })
+    cover.addEventListener("mousemove", function(event){
+        if (current !== null) {
+            const x = event.clientX;
+            const y = event.clientY;
+            current.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
+        }
+    })
+    document.getElementById("sidebar").appendChild(addGlyph("you"));
 }

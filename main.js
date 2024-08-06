c23ac0ec379968e183b9349ee8b2170ae5c2b179
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-08-06 11:00:54 +1000

    Added a cover so you will always be having your mouse over the glyph to have very smooth movement!

--- main.js
+++ main.js
@@ -2,8 +2,24 @@
 
 var offset = [0,0];
 var reload = true;
+var current = null;
+var cover;
 
 document.body.onload = function() {
+    cover = document.getElementById("cover");
+    cover.style = "display: none;";
+    cover.addEventListener("mouseup", function(event){
+        cover.style = "display: none;";
+        reload = true;
+        current = null;
+    })
+    cover.addEventListener("mousemove", function(event){
+        if (current !== null) {
+            const x = event.clientX;
+            const y = event.clientY;
+            current.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
+        }
+    })
     var it = document.getElementById("glyph")
     it.ondragstart = function() { return false; };
     it.addEventListener('mousemove', function(event) {
@@ -11,6 +27,10 @@ document.body.onload = function() {
         const y = event.clientY;
         var flags = event.buttons !== undefined ? event.buttons : event.which;
         if ((flags & 1) === 1) {
+            if (current === null) {
+                current = it;
+                cover.style = "display: block;";
+            }
             if (reload) {
                 // Set offset to mouse position - current position of object, if set
                 offset = [x - it.offsetLeft, y - it.offsetTop];
@@ -19,5 +39,9 @@ document.body.onload = function() {
             it.style = `top: ${y-offset[1]};left: ${x-offset[0]}`
         }
     });
-    it.addEventListener("mouseup", function(event){reload = true;});
+    it.addEventListener("mouseup", function(event){
+        cover.style = "display: none;";
+        reload = true;
+        current = null;
+    });
 }
